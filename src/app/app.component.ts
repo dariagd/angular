843c9b16c81720d2af6052fdb444b2a665a64b71
daryagd <grishinadaryadm@gmail.com>
2021-12-26 16:52:43 +0300

    form with name and greeting name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать {{name}}!</h1>`
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  name = ''
+  // title = 'frontend';
 }
