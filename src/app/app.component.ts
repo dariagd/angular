0c2cbda07880243a4379f9d0a69de11ca7f82d44
daryagd <grishinadaryadm@gmail.com>
2021-12-26 17:48:30 +0300

    hello angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
-  // styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = ''
